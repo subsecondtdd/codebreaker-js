ba4f7ec93ab81931c84806aa70c4ed69db53daa6
Josh Chisholm <joshuachisholm@gmail.com>
2018-04-22 00:11:01 +0100

    Submitting forms, but how do we synchronise?

--- interfaces/web/WebApp.js
+++ interfaces/web/WebApp.js
@@ -1,10 +1,39 @@
 module.exports = class WebApp {
+  constructor({ controllers }) {
+    this._controllers = controllers;
+  }
+
   async get(path) {
-    return {
-      body: `<form action="/games" data-action="startGameWithWord" onsubmit="return false">
-          <input name="word" />
-          <input type="submit" value="Start Game" />
-        </form>`
-    };
+    if (path === "/") {
+      return {
+        body: `
+           <form action="/games" data-action="startGameWithWord">
+            <input type="text" name="word" />
+            <input type="submit" value="Start Game" />
+          </form>
+          `
+      };
+    }
+    throw new Error(`GET '${path}' is not implemented`);
+  }
+
+  async post(path, params) {
+    if (path === "/games") {
+      await this._performAction({ action: "startGameWithWord", params });
+    } else {
+      throw new Error(`POST '${path}' is not implemented`);
+    }
+  }
+
+  async _performAction({ action, params }) {
+    const controller = this._controllers.findControllerWithAction(action);
+    const rendering = await controller[action].call(controller, params);
+    console.log("rendering", rendering);
+    if (!rendering) {
+      throw new Error(`Action ${action} returned ${typeof rendering}`);
+    }
+    if (rendering.stream) {
+      throw new Error("Coming soon!");
+    }
   }
 };
--- interfaces/web/WebAppDomRenderer.js
+++ interfaces/web/WebAppDomRenderer.js
@@ -1,10 +1,29 @@
+const baseUrl = "http://web-app-dom-renderer";
+
 module.exports = class WebAppDomRenderer {
   constructor({ webApp }) {
-    this._webApp = webApp
+    this._webApp = webApp;
+    const base = document.createElement("base");
+    base.href = baseUrl;
+    document.head.appendChild(base);
   }
 
   async renderWebAppInElement(domElement) {
-    const response = await this._webApp.get('/')
-    domElement.innerHTML = response.body
+    const response = await this._webApp.get("/");
+    domElement.innerHTML = response.body;
+    domElement.querySelectorAll("form").forEach(form => {
+      form.addEventListener("submit", event => {
+        event.preventDefault();
+        const path = event.target.action.substring(baseUrl.length);
+        const params = {};
+        const inputs = [].slice.apply(
+          form.querySelectorAll('input[type="text"]')
+        );
+        inputs.forEach(input => {
+          params[input.getAttribute("name")] = input.value;
+        });
+        this._webApp.post(path, params);
+      });
+    });
   }
-}
+};
