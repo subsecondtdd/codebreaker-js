5e56704da2ea41b997924c284b8034989f1c5294
Josh Chisholm <joshuachisholm@gmail.com>
2018-05-14 06:02:25 +0100

    Invert container registration

--- features/support/world.js
+++ features/support/world.js
@@ -1,52 +1,10 @@
 const { setWorldConstructor } = require("cucumber");
-const {
-  Container,
-  Assembly,
-  characters,
-  ports,
-  sessions,
-  controllers,
-  web,
-  domApps
-} = require("../../lib");
+const buildContainer = require("../../lib");
 
 class World {
   constructor() {
     this._cast = {};
-    this._container = new Container(new Assembly());
-    Object.keys(characters).forEach(role =>
-      this._container.register({
-        role,
-        Constructor: characters[role]
-      })
-    );
-    Object.keys(ports).forEach(role =>
-      ports[role].forEach(Constructor =>
-        this._container.register({ role, Constructor, scope: "singleton" })
-      )
-    );
-    Object.keys(web).forEach(role =>
-      this._container.register({
-        role,
-        Constructor: web[role],
-        scope: "singleton"
-      })
-    );
-    sessions.forEach(Constructor =>
-      this._container.register({
-        role: "session",
-        Constructor
-      })
-    );
-    this._container.register({
-      role: "domApps",
-      Constructor: domApps,
-      scope: "singleton"
-    });
-    this._container.register({
-      role: "controllers",
-      Constructor: controllers(this._container)
-    });
+    this._container = buildContainer();
   }
 
   findOrCreateCharacter({ roleName, characterName }) {
