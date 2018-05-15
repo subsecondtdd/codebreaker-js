1e8bccfac573adc5baa74841122b93590e26f2e2
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2018-05-15 13:03:20 +0100

    Logic kind of working

--- features/support/World.js
+++ features/support/World.js
@@ -6,13 +6,13 @@ const CodeBreakerController = require("../../lib/CodeBreakerController");
 class World {
   constructor() {
     this._cast = {};
+    this._controller = new CodeBreakerController();
   }
 
   findOrCreatePlayer(playerName) {
     if (this._cast[playerName]) return this._cast[playerName];
 
-    const controller = new CodeBreakerController();
-    const session = new DirectSession({ controller });
+    const session = new DirectSession({ controller: this._controller });
     const player = new Player({ session });
     this._cast[playerName] = player;
     return player;
